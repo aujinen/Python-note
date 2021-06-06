4ab4903bbca78342f73daf5b8453b981fa353fd2
H.Nishiyama <55919851+aujinen@users.noreply.github.com>
2021-06-06 14:22:26 +0900

    Create UTF-8-with_or_without_BOM.py

---
+++ UTF-8-with_or_without_BOM.py
@@ -0,0 +1,19 @@
+import csv
+print("without-BOM-UTF8 / open with utf_8")
+csv_file = open("./woBOM.csv","r",encoding="utf_8")
+f = csv.DictReader(csv_file)
+for row in f:
+    print(row)
+csv_file.close()
+print("with-BOM-UTF8 / open with utf_8")
+csv_file = open("./withBOM.csv","r",encoding="utf_8")
+f = csv.DictReader(csv_file)
+for row in f:
+    print(row)
+csv_file.close()
+print("with-BOM-UTF8 / open with utf_8_sig")
+csv_file = open("./withBOM.csv","r",encoding="utf_8_sig")
+f = csv.DictReader(csv_file)
+for row in f:
+    print(row)
+csv_file.close()
